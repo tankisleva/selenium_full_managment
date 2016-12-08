19bdbf155085e076d0e906bacd29a1d2e26a4b35
Oleg <prostooleg1@gmail.com>
2016-12-08 15:41:26 +0300

    Проверка, что все ссылки открываются в новом окне

---
+++ java-example/src/test/java/ru/stqa/study/selenium/TestVerifyNewWindow.java
@@ -0,0 +1,43 @@
+package ru.stqa.study.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by oleg on 08.12.16.
+ */
+public class TestVerifyNewWindow extends TestBase {
+
+
+    @Test
+    public void testVerifyNewWindow() throws Exception {
+        openLiteCartAdmin();
+        loginLiteCart("admin", "admin");
+        click(By.cssSelector("a[href*='?app=countries&doc=countries'] span.name"));
+        sleep(500);
+        click(By.cssSelector("a.button"));
+        sleep(500);
+        List<WebElement> windowElements =  driver.findElements(By.cssSelector("i.fa.fa-external-link"));
+
+        for (WebElement windowElement: windowElements){
+            String originalWindow = driver.getWindowHandle();
+            Set<String> oldWindows = driver.getWindowHandles();
+            windowElement.click();
+            Set <String>  newWindows =  driver.getWindowHandles();
+            newWindows.removeAll(oldWindows);
+            String newWindow  = newWindows.iterator().next();
+            driver.switchTo().window(newWindow);
+            driver.close();
+            driver.switchTo().window(originalWindow);
+        }
+
+    }
+
+
+}
